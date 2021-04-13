14f2087a9220e9786c97c5ade1dc03f162befc61
Legendary Soldier <76736800+LegendarySoldier@users.noreply.github.com>
2021-04-13 19:06:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var Modal = document.getElementedById("modal fade");
+var closeBtn = document.getElementedById("closeButton");
+var modalBtn = document.getElementedById("ModalBtn1");
+
+modalBtn.addEventListener("click", openModal);
+
+function openModal(){
+   console.log(123) 
+}
+
+
